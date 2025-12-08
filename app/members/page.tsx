882af7650246c3afa1113b48e6ea98d3d98465b4
itohkazusyou1165-ux <itohkazusyou1165@gmail.com>
2025-12-08 13:56:53 +0900

    1208_P92まで

---
+++ app/members/page.tsx
@@ -0,0 +1,10 @@
+import { Margarine } from "next/font/google";
+import React from "react";
+
+export default function page() {
+  return (
+    <div style={{ margin: 20 }}>
+      <h1>メンバーページ</h1>
+    </div>
+  );
+}
