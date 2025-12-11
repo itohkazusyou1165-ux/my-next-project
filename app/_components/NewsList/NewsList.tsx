f004c62277d31f4255e56ab74343cd287f053a09
itohkazusyou1165-ux <itohkazusyou1165@gmail.com>
2025-12-11 11:21:44 +0900

    5章まで完了

--- app/_components/NewsList/NewsList.tsx
+++ app/_components/NewsList/NewsList.tsx
@@ -6,39 +6,47 @@ import Category from "../Category/Category";
 import Link from "next/link";
 
 type Props = {
-    news:News[];
+  news: News[];
 };
 
-
-export default function NewsList({news}: Props) {
-if (news.length === 0){
+export default function NewsList({ news }: Props) {
+  if (news.length === 0) {
     return <p>記事がありません</p>;
-}
+  }
 
   return (
-      <ul>
-        {news.map((article) =>(
-          <li key={article.id} className={styles.list}>
-            <Link href={`/news/${article.id}`} className={styles.link}>
+    <ul>
+      {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
+            {article.thumbnail ? (
               <Image
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
+                src={article.thumbnail.url}
+                alt="サムネイル画像"
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
               />
-              <dl className={styles.content}>
-                <dt className={styles.newsItemTitle}>
-                  {article.title}
-                </dt>
-                <dd className={styles.meta}>
-                    <Category category={article.category}/>
-                    <Date date={article.publishedAt ?? article.createdAt} />
-                </dd>
-              </dl>
-            </Link>
-          </li>
-        ))}
-      </ul>
+            ) : (
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            )}
+
+            <dl className={styles.content}>
+              <dt className={styles.newsItemTitle}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
