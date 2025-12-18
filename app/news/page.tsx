431ba2dd5013c27a9752957f7b79cee092650b71
itohkazusyou1165-ux <itohkazusyou1165@gmail.com>
2025-12-18 10:25:56 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,6 +3,7 @@ import { getNewsList } from "../_libs/microcms";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination/Pagination";
 import SearchField from "../_components/SearchField/SearchField";
+export const revalidate = 0;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
