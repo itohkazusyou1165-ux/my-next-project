9d7e1ab7bda0bcf155bffed850c93c4afa9d780c
itohkazusyou1165-ux <itohkazusyou1165@gmail.com>
2025-12-18 11:08:22 +0900

    サイト全体にrevalidateを設定

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,7 +3,8 @@ import { getNewsList } from "../_libs/microcms";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination/Pagination";
 import SearchField from "../_components/SearchField/SearchField";
-export const revalidate = 60;
+
+
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
